7bf7eff6724b22a918310a9b478cf163dc95166f
jeanmatheuss <jeanmat42@gmail.com>
2025-08-29 10:10:42 -0300

    feat: exercícios

--- exercicios/exercicios.sql
+++ exercicios/exercicios.sql
@@ -45,7 +45,7 @@ WHERE IdProduto = 15;
 -- Listar todas as transações adicionando uma coluna nova sinalizando “alto”, “médio” e “baixo” para o valor dos pontos [<10 ; <500; >=500]
 SELECT *,
 		CASE
-			WHEN QtdePontos <= 10 THEN 'Baixo'
+			WHEN QtdePontos < 10 THEN 'Baixo'
 			WHEN QtdePontos < 500 THEN 'Médio'
 			ELSE 'Alto'
 		END as Grupos
@@ -53,3 +53,110 @@ SELECT *,
 
 FROM transacoes
 LIMIT 10;
+
+-- Quantos clientes tem email cadastrado?
+SELECT sum(FlEmail)
+
+FROM clientes;
+
+--Qual cliente juntou mais pontos positivos em 2025-05?
+SELECT IdCliente,
+		sum(QtdePontos) as TotalPts
+
+FROM transacoes
+
+WHERE DtCriacao >= '2025-05-01'
+AND DtCriacao < '2025-06-01'
+AND QtdePontos > 0
+
+GROUP BY IdCliente
+
+ORDER BY sum(QtdePontos) DESC
+LIMIT 1;
+
+-- Qual cliente fez mais transações no ano de 2024?
+
+SELECT IdCliente,
+		count(DISTINCT IdTransacao) as TotalTransacoes
+
+FROM transacoes
+
+WHERE DtCriacao >= '2024-01-01'
+AND DtCriacao < '2025-01-01'
+
+
+GROUP BY IdCliente
+
+ORDER BY count(IdTransacao) DESC;
+
+-- Quantos produtos são de rpg?
+
+SELECT count(*)
+
+FROM produtos
+
+WHERE DescCateogriaProduto = 'rpg';
+
+SELECT DescCateogriaProduto,
+		count(*)
+
+FROM produtos
+
+GROUP BY DescCateogriaProduto;
+
+-- Qual o valor médio de pontos positivos por dia?
+SELECT sum(QtdePontos) as totalPts,
+		count(DISTINCT substr(DtCriacao, 1, 10)) as qtdDiasUnicos,
+		sum(QtdePontos) / count(DISTINCT substr(DtCriacao, 1, 10)) as avgPontosDia
+
+FROM transacoes
+
+WHERE QtdePontos > 0;
+
+SELECT substr(DtCriacao, 1, 10) as dtDia,
+		avg(QtdePontos) as avgPontosDia
+
+FROM transacoes
+
+WHERE QtdePontos > 0
+-- primeira coluna do select = 1
+GROUP BY 1 
+ORDER BY 1
+
+LIMIT 1;
+
+-- Qual dia da semana quem mais pedidos em 2025?
+SELECT	
+		strftime('%w', substr(DtCriacao, 1, 10)) as diaSemana,
+		count(DISTINCT IdTransacao) as QtdTransacoes
+		
+
+FROM transacoes
+
+WHERE substr(DtCriacao, 1, 4) = '2025'
+
+GROUP BY 1
+
+-- Qual o produto mais transacionado?
+
+SELECT IdProduto,
+	--	count(*)
+		sum(QtdeProduto) as qtdProdutosSum
+
+FROM transacao_produto
+
+GROUP BY IdProduto
+ORDER BY count(*) DESC
+
+LIMIT 1;
+
+-- Qual o produto com mais pontos transacionados?
+
+SELECT IdProduto,
+		sum(VlProduto * QtdeProduto) AS totalPontos,
+		sum(QtdeProduto) as qtdVendas
+
+FROM transacao_produto
+
+GROUP BY IdProduto
+ORDER BY sum(VlProduto)  DESC
