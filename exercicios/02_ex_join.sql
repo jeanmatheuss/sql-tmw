3d24f86458e6f50e4fb38bde63cca754c0611c1c
jeanmatheuss <jeanmat42@gmail.com>
2025-09-01 10:00:04 -0300

    feat: new data

--- exercicios/02_ex_join.sql
+++ exercicios/02_ex_join.sql
@@ -55,18 +55,82 @@
 
 -- Qual o total de pontos trocados no Stream Elements em Junho de 2025?
 
-SELECT sum(t2.VlProduto),
-        t3.DescCateogriaProduto
+-- SELECT sum(t2.VlProduto),
+--         t3.DescCateogriaProduto
 
-FROM transacoes as t1
+-- FROM transacoes as t1
 
-LEFT JOIN transacao_produto as t2
-ON t1.IdTransacao = t2.IdTransacao
+-- LEFT JOIN transacao_produto as t2
+-- ON t1.IdTransacao = t2.IdTransacao
 
-LEFT JOIN produtos as t3
-ON t2.IdProduto = t3.IdProduto
+-- LEFT JOIN produtos as t3
+-- ON t2.IdProduto = t3.IdProduto
 
-WHERE t3.IdProduto IN ('14','17')
-AND t2.VlProduto > 0
-GROUP BY t3.IdProduto
+-- WHERE t3.IdProduto IN ('14','17')
+-- AND t2.VlProduto > 0
+-- GROUP BY t3.IdProduto
 
+-- Quais clientes mais perderam pontos por Lover?
+-- SELECT IdCliente,
+--         sum(VlProduto)
+
+-- FROM transacao_produto as t1
+
+-- LEFT JOIN produtos as t2
+-- ON t1.IdProduto = t2.IdProduto
+
+-- LEFT JOIN transacoes as t3
+-- ON t1.IdTransacao = t3.IdTransacao
+
+-- WHERE t2.DescCateogriaProduto = 'lovers'
+
+-- GROUP BY t3.IdCliente
+-- ORDER BY sum(VlProduto) ASC;
+
+-- Quais clientes assinaram a lista de presença no dia 2025/08/25?
+
+-- SELECT t3.IdCliente,
+--         t2.DescProduto
+
+-- FROM transacao_produto as t1
+
+-- LEFT JOIN produtos as t2
+-- ON t1.IdProduto = t2.IdProduto
+
+-- LEFT JOIN transacoes as t3
+-- ON t1.IdTransacao = t3.IdTransacao
+
+-- WHERE t2.DescProduto = 'Lista de presença'
+-- AND substr(t3.DtCriacao, 1, 10) = '2025-08-25';
+
+-- Do início ao fim do nosso curso (2025/08/25 a 2025/08/29), quantos clientes assinaram a lista de presença?
+
+-- SELECT count(DISTINCT t3.IdCliente)
+
+-- FROM transacao_produto as t1
+
+-- LEFT JOIN produtos as t2
+-- ON t1.IdProduto = t2.IdProduto
+
+-- LEFT JOIN transacoes as t3
+-- ON t1.IdTransacao = t3.IdTransacao
+
+-- WHERE t2.DescProduto = 'Lista de presença'
+-- AND substr(t3.DtCriacao, 1, 10) >= '2025-08-25'
+-- AND substr(t3.DtCriacao, 1, 10) < '2025-08-30'
+
+--Clientes mais antigos, tem mais frequência de transação?
+SELECT t1.IdCliente,
+        -- julianday(substr(DtCriacao, 1,19)),
+        -- julianday('now'),
+        -- cast -> transforma pra int
+        CAST(julianday('now') - julianday(substr(t1.DtCriacao, 1,19))AS INT) as IdadeBase,
+        count(t2.IdTransacao) as QtdTransacoes
+
+        
+FROM clientes as t1
+
+LEFT JOIN transacoes as t2
+ON t1.IdCliente = t2.IdCliente
+
+GROUP BY t1.IdCliente, idadeBase
